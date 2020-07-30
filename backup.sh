5e24694624e188b1db572c2da866fb46c47cd61d
src <src@srcw.net>
2020-07-30 13:48:18 +0900

    init

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ TIMESTAMP=$1
 basename=db_"$TIMESTAMP.sql.bz2"
 filename=$basename
 
-echo "dump sakuravps2g database to $filename"
+echo "dump mysql all database to $filename"
 
 nice mysqldump --single-transaction --default-character-set=binary --quick --all-databases | bzip2 > $filename
 
